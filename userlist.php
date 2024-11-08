e0d771ba44dee972eb5cd8ce8cced30f0ccc3df6
pawanaditya369 <pawanaditya85@gmail.com>
2024-11-08 00:37:29 -0800

    Update userlist.php

--- userlist.php
+++ userlist.php
@@ -1,28 +1,23 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 function fetch_users($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Not recommended for production, only use if SSL/TLS certificates are not configured
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
-    if (curl_errno($ch)) {
-        echo 'Curl error: ' . curl_error($ch);
+    if (!$result) {
+        die('Curl error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
     }
     curl_close($ch);
     return json_decode($result, true);
 }
 
-// Adjust the URLs to point to where the files are actually hosted
-$companyA_users = fetch_users('http://pawanaditya.tech/users.php'); // Your server
-//$companyB_users = fetch_users('https://ninjacoder.tech/users/get_local_users.php'); // Teammate's domain for Company B
-//$companyC_users = fetch_users('http://teammateCdomain.com/companyC.php'); // Teammate's domain for Company C
-
-$all_users = array_merge($companyA_users, $companyB_users, $companyC_users);
-
-echo "<ul>";
-foreach ($all_users as $user) {
-    echo "<li>$user</li>";
-}
-echo "</ul>";
+$companyA_users = fetch_users('http://pawanaditya.tech/users.php');
+echo "<pre>";
+print_r($companyA_users);
+echo "</pre>";
 ?>
